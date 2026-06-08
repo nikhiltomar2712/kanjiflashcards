a0651e513d7d497aa77569a7f032ef08d93c4ca4
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-06-08 13:02:52 +0530

    Create n4.js

---
+++ data/n4.js
@@ -0,0 +1,205 @@
+const n4Raw = `
+不|―|フ、ブ|not; negative
+世|よ|セイ、セ|world; generation
+主|ぬし、おも|シュ|master; main
+事|こと|ジ|thing; matter
+乗|の(る)|ジョウ|ride; board
+京|―|キョウ|capital
+仕|つか(える)|シ、ジ|serve; work
+代|か(わる)、よ|ダイ、タイ|substitute; era
+以|―|イ|by means of; because
+低|ひく(い)|テイ|low
+住|す(む)|ジュウ|live; reside
+体|からだ|タイ|body
+使|つか(う)|シ|use
+便|たよ(り)|ベン、ビン|convenience; mail
+借|か(りる)|シャク|borrow
+働|はたら(く)|ドウ|work
+元|もと|ゲン、ガン|origin; former
+兄|あに|キョウ、ケイ|older brother
+光|ひかり|コウ|light
+公|おおやけ|コウ|public
+内|うち|ナイ|inside
+冷|つめ(たい)、ひ(える)|レイ|cold
+切|き(る)|セツ|cut
+別|わか(れる)|ベツ|separate
+力|ちから|リョク|power; strength
+勉|―|ベン|study; effort
+動|うご(く)|ドウ|move
+医|―|イ|medicine; doctor
+区|―|ク|ward; district
+卒|―|ソツ|graduate
+協|―|キョウ|cooperation
+去|さ(る)|キョ、コ|leave; past
+取|と(る)|シュ|take
+口|くち|コウ、ク|mouth
+合|あ(う)|ゴウ|fit; join
+同|おな(じ)|ドウ|same
+味|あじ|ミ|taste; flavor
+品|しな|ヒン|goods; item
+員|―|イン|member
+問|と(う)、とん|モン|question; ask
+回|まわ(る)|カイ|times; revolve
+図|はか(る)|ズ、ト|diagram; map
+地|―|チ、ジ|ground; earth
+堂|―|ドウ|hall; temple
+場|ば|ジョウ|place
+売|う(る)|バイ|sell
+夏|なつ|カ|summer
+夕|ゆう|セキ|evening
+多|おお(い)|タ|many
+夜|よる|ヤ|night
+夢|ゆめ|ム|dream
+妹|いもうと|マイ|younger sister
+姉|あね|シ|older sister
+始|はじ(める)|シ|begin
+字|あざ|ジ|character; letter
+季|―|キ|season
+学|まな(ぶ)|ガク|study; learning
+完|―|カン|complete
+室|むろ|シツ|room
+家|いえ、うち|カ、ケ|house; family
+寒|さむ(い)|カン|cold
+寺|てら|ジ|temple
+屋|や|オク|shop; roof
+工|―|コウ、ク|craft; industry
+市|いち|シ|city; market
+帰|かえ(る)|キ|return
+広|ひろ(い)|コウ|wide; spacious
+度|たび|ド|degree; time
+建|た(てる)|ケン|build
+引|ひ(く)|イン|pull
+強|つよ(い)|キョウ|strong
+当|あ(たる)|トウ|hit; right
+形|かたち|ケイ|shape
+待|ま(つ)|タイ|wait
+徒|―|ト|junior; follower
+必|かなら(ず)|ヒツ|necessarily
+忙|いそが(しい)|ボウ|busy
+思|おも(う)|シ|think
+急|いそ(ぐ)|キュウ|hurry
+悪|わる(い)|アク|bad
+意|―|イ|idea; mind
+成|な(る)|セイ|become
+戦|いくさ|セン|war; battle
+所|ところ|ショ|place
+持|も(つ)|ジ|hold; have
+教|おし(える)|キョウ|teach
+文|ふみ|ブン、モン|writing; sentence
+料|―|リョウ|fee; material
+新|あたら(しい)|シン|new
+旅|たび|リョ|travel
+族|―|ゾク|tribe; family
+早|はや(い)|ソウ|early; fast
+明|あか(るい)、あ(ける)|メイ|bright; clear
+映|うつ(る)|エイ|reflect; project
+春|はる|シュン|spring
+昼|ひる|チュウ|daytime
+暑|あつ(い)|ショ|hot
+暗|くら(い)|アン|dark
+曜|―|ヨウ|day of week
+有|あ(る)|ユウ|exist; have
+服|―|フク|clothes
+朝|あさ|チョウ|morning
+村|むら|ソン|village
+林|はやし|リン|grove; forest
+森|もり|シン|forest
+楽|たの(しい)|ガク、ラク|comfort; music
+横|よこ|オウ|side; horizontal
+欠|か(ける)|ケツ|lack
+死|し(ぬ)|シ|death
+注|そそ(ぐ)|チュウ|pour; note
+洋|―|ヨウ|ocean; western
+洗|あら(う)|セン|wash
+海|うみ|カイ|sea
+泳|およ(ぐ)|エイ|swim
+深|ふか(い)|シン|deep
+温|あたた(かい)|オン|warm
+港|みなと|コウ|harbor
+湯|ゆ|トウ|hot water
+漢|―|カン|Chinese
+牛|うし|ギュウ|cow
+物|もの|ブツ|thing
+特|―|トク|special
+犬|いぬ|ケン|dog
+理|ことわり|リ|reason; logic
+産|う(む)|サン|produce; birth
+用|もち(いる)|ヨウ|use; business
+田|た|デン|rice field
+町|まち|チョウ|town
+画|―|ガ、カク|picture
+界|―|カイ|world
+病|やまい|ビョウ|illness
+発|―|ハツ|depart; emit
+登|のぼ(る)|トウ、ト|climb
+的|まと|テキ|target; -like
+着|き(る)、つ(く)|チャク|arrive; wear
+知|し(る)|チ|know
+短|みじか(い)|タン|short
+研|―|ケン|polish; research
+私|わたし|シ|I; private
+秋|あき|シュウ|autumn
+科|―|カ|department; subject
+究|―|キュウ|research
+答|こた(える)|トウ|answer
+紙|かみ|シ|paper
+終|お(わる)|シュウ|end
+習|なら(う)|シュウ|learn
+考|かんが(える)|コウ|think; consider
+者|もの|シャ|person
+肉|―|ニク|meat
+自|みずか(ら)|ジ、シ|self
+色|いろ|ショク|color
+花|はな|カ|flower
+英|―|エイ|English; hero
+茶|―|チャ、サ|tea
+菜|な|サイ|vegetable
+薬|くすり|ヤク|medicine
+親|おや、した(しい)|シン|parent; intimate
+言|い(う)、こと|ゲン、ゴン|say; word
+計|はか(る)|ケイ|measure; plan
+記|しる(す)|キ|write down; record
+試|こころ(みる)|シ|test; try
+説|と(く)|セツ|theory; explain
+読|よ(む)|ドク|read
+談|―|ダン|discuss
+買|か(う)|バイ|buy
+貸|か(す)|タイ|lend
+質|―|シツ|quality
+赤|あか(い)|セキ|red
+走|はし(る)|ソウ|run
+起|お(きる)|キ|wake up; occur
+転|ころ(ぶ)|テン|roll; change
+軽|かる(い)|ケイ|light (weight)
+近|ちか(い)|キン|near
+送|おく(る)|ソウ|send
+通|とお(る)|ツウ|pass through
+進|すす(む)|シン|advance
+遅|おそ(い)|チ|slow; late
+遊|あそ(ぶ)|ユウ|play
+運|はこ(ぶ)|ウン|carry; luck
+道|みち|ドウ|road; way
+遠|とお(い)|エン|far
+都|みやこ|ト、ツ|metropolis; capital
+重|おも(い)、かさ(ねる)|ジュウ|heavy; layer
+野|の|ヤ|field; wild
+銀|―|ギン|silver
+開|あ(ける)、ひら(く)|カイ|open
+間|あいだ、ま|カン、ケン|interval; between
+関|せき|カン|barrier; related
+集|あつ(まる)|シュウ|gather
+青|あお(い)|セイ|blue
+音|おと、ね|オン|sound
+頭|あたま|トウ、ズ|head
+顔|かお|ガン|face
+風|かぜ|フウ|wind
+飯|めし|ハン|meal; cooked rice
+飲|の(む)|イン|drink
+館|やかた|カン|building; hall
+駅|―|エキ|station
+験|―|ケン|test; verify
+鳥|とり|チョウ|bird
+麦|むぎ|バク|wheat; barley
+黄|き|コウ、オウ|yellow
+黒|くろ(い)|コク|black
+`;
