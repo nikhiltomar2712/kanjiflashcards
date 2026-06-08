56395636355bc5eda5390c60c8e2d6e4b6d0bd10
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-06-08 13:02:23 +0530

    Create n5.js

---
+++ data/n5.js
@@ -0,0 +1,123 @@
+const n5Raw = `
+一|ひと(つ)|イチ|one
+二|ふた(つ)|ニ|two
+三|み(つ)|サン|three
+四|よ(つ)、よん|シ|four
+五|いつ(つ)|ゴ|five
+六|む(つ)|ロク|six
+七|なな(つ)、なな|シチ|seven
+八|や(つ)|ハチ|eight
+九|ここの(つ)|キュウ、ク|nine
+十|とお|ジュウ|ten
+百|もも|ヒャク|hundred
+千|ち|セン|thousand
+万|よろず|マン、バン|ten thousand
+円|まる(い)|エン|circle; yen
+日|ひ、か|ニチ、ジツ|day; sun
+月|つき|ゲツ、ガツ|month; moon
+火|ひ|カ|fire
+水|みず|スイ|water
+木|き|ボク、モク|tree; wood
+金|かね|キン、コン|gold; money
+土|つち|ド、ト|earth; soil
+年|とし|ネン|year
+時|とき|ジ|time; hour
+間|あいだ、ま|カン、ケン|interval; between
+上|うえ、あ(げる)|ジョウ|up; above
+下|した、さ(げる)|カ、ゲ|down; below
+左|ひだり|サ|left
+右|みぎ|ウ、ユウ|right
+中|なか|チュウ|middle; inside
+外|そと、ほか|ガイ、ゲ|outside
+前|まえ|ゼン|before; in front
+後|あと、うし(ろ)|ゴ、コウ|after; behind
+北|きた|ホク|north
+南|みなみ|ナン、ナ|south
+東|ひがし|トウ|east
+西|にし|セイ、サイ|west
+大|おお(きい)|ダイ、タイ|big
+小|ちい(さい)|ショウ|small
+高|たか(い)|コウ|tall; expensive
+安|やす(い)|アン|cheap; peaceful
+新|あたら(しい)|シン|new
+古|ふる(い)|コ|old
+多|おお(い)|タ|many
+少|すく(ない)、すこ(し)|ショウ|few; little
+長|なが(い)|チョウ|long
+短|みじか(い)|タン|short
+白|しろ(い)|ハク、ビャク|white
+黒|くろ(い)|コク|black
+赤|あか(い)|セキ|red
+青|あお(い)|セイ|blue
+名|な|メイ、ミョウ|name
+人|ひと|ジン、ニン|person
+子|こ|シ、ス|child
+女|おんな|ジョ、ニョ|woman; female
+男|おとこ|ダン、ナン|man; male
+父|ちち|フ|father
+母|はは|ボ|mother
+友|とも|ユウ|friend
+先|さき|セン|previous; ahead
+生|い(きる)、う(まれる)|セイ、ショウ|life; birth
+学|まな(ぶ)|ガク|study; learning
+校|―|コウ|school
+本|もと|ホン|book; origin
+何|なに、なん|カ|what
+毎|ごと|マイ|every
+半|なか(ば)|ハン|half
+分|わ(ける)|ブン、フン|minute; part
+今|いま|コン、キン|now
+休|やす(む)|キュウ|rest
+会|あ(う)|カイ|meeting
+社|やしろ|シャ|company; shrine
+電|―|デン|electricity
+車|くるま|シャ|car; vehicle
+話|はな(す)、はなし|ワ|talk; story
+聞|き(く)|ブン、モン|listen; ask
+読|よ(む)|ドク|read
+書|か(く)|ショ|write
+見|み(る)|ケン|see
+行|い(く)、ゆ(く)|コウ、ギョウ|go
+来|く(る)|ライ|come
+出|で(る)、だ(す)|シュツ|exit; put out
+入|い(る)、はい(る)|ニュウ|enter
+食|た(べる)|ショク|eat
+飲|の(む)|イン|drink
+買|か(う)|バイ|buy
+語|かた(る)|ゴ|language; word
+国|くに|コク|country
+山|やま|サン|mountain
+川|かわ|セン|river
+林|はやし|リン|grove; forest
+森|もり|シン|forest
+空|そら|クウ|sky; empty
+天|あめ、あま|テン|heaven
+気|―|キ、ケ|spirit; energy
+雨|あめ|ウ|rain
+花|はな|カ|flower
+石|いし|セキ|stone
+田|た|デン|rice field
+牛|うし|ギュウ|cow
+馬|うま|バ|horse
+鳥|とり|チョウ|bird
+魚|さかな|ギョ|fish
+肉|―|ニク|meat
+米|こめ|ベイ、マイ|rice (grain)
+麦|むぎ|バク|wheat; barley
+茶|―|チャ、サ|tea
+店|みせ|テン|shop; store
+駅|―|エキ|station
+道|みち|ドウ|road; way
+地|―|チ、ジ|ground; earth
+図|はか(る)|ズ、ト|diagram; map
+番|―|バン|number; turn
+号|―|ゴウ|number; edition
+口|くち|コウ、ク|mouth
+目|め|モク、ボク|eye
+耳|みみ|ジ|ear
+手|て|シュ|hand
+足|あし|ソク|foot; leg
+体|からだ|タイ|body
+力|ちから|リョク|power; strength
+円|まる(い)|エン|circle; yen
+`;
